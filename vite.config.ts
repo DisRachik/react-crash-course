87c7f12e3c3d5b32716fd3fd44133ff2131f5c6a
DisRachik <radchenkoig@gmail.com>
2024-09-04 16:30:15 +0300

    add adjust in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/react-crash-course/',
   plugins: [react()],
-})
+  build: {
+    sourcemap: true,
+  },
+  resolve: {
+    alias: {
+      src: '/src',
+      components: '/src/components',
+    },
+  },
+});
